f4d8f3a4e238d5f7cd7a9fea78037cea47b9d5a6
anirudhlohi <74246267+anirudhlohi@users.noreply.github.com>
2021-01-08 16:32:44 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+var first = "My first console project";
+console.log(first);
+var name1 = "anirudh";
+var last = "lohia";
+console.log(name1.concat(last)) 
+var number1 = 12;
+var number2 = 1;
+console.log(number1 + number2)
+console.log(number1 - number2)
+console.log(number1 * number2)
+console.log(number1 / number2)
